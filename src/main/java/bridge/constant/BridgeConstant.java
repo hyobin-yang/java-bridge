24ba7fff197dad1933d28e8a54f09a0d1547b5a1
양효빈 <hyobin3026@naver.com>
2024-12-13 16:30:33 +0900

    feat(BridgeConstant): 다리 관련 상수 클래스 구현

---
+++ src/main/java/bridge/constant/BridgeConstant.java
@@ -0,0 +1,10 @@
+package bridge.constant;
+
+public class BridgeConstant {
+
+    public static final int MINIMUM_BRIDGE_SIZE = 3;
+    public static final int MAXIMUM_BRIDGE_SIZE = 20;
+
+    private BridgeConstant(){
+    }
+}
